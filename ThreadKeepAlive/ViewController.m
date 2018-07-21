8fdb144df0b0fab0396fa75b2e8d36b33ed59c6a
王辉平 <WANGHUIPING032@pingan.com.cn>
2018-07-21 19:09:19 +0800

    新疆线程保活工程

---
+++ ThreadKeepAlive/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ThreadKeepAlive
+//
+//  Created by 王辉平 on 2018/7/21.
+//  Copyright © 2018年 王辉平. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
